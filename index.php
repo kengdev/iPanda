46a64015fa395179e4ebd9d8be8cd065f19798bc
kengdev <48957697+kengdev@users.noreply.github.com>
2021-10-30 13:37:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ if (isset($input['entry'][0]['messaging'][0]['sender']['id'])) {
     $ch = curl_init($url);
 
     /*prepare response*/
-    $message = 'What sup man';
+    $message = 'What sup man'. $sender;
 
     $resp = array(
       'recipient' => array(
