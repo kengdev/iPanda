37ca6ecf64ee812fa4b7259b2608a665307b8eb8
kengdev <48957697+kengdev@users.noreply.github.com>
2021-10-30 13:35:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 $access_token = 'EABDpq01cuYMBANUycT4UYnYDVaqxLpZBlmjeR46MDdEGQb2laU8ZBXMX6aRgv7bj6NBRiyPoXP0nGzaKPotBZAw77VCCGkGim8QXhPHYnw6bDgQHZB5ZCa7SRUaGdTZAb8k36b1Aad5L3PWX2zvo5qtwBkehd6pJ4JY6NHcucvkQAN9BHJmegvf4hBmPntjJF8qi0HNUS1kwZDZD';
 
+
 /* validate verify token needed for setting up web hook */ 
 
 if (isset($_GET['hub_verify_token'])) { 
@@ -16,10 +17,10 @@ if (isset($_GET['hub_verify_token'])) {
 
 /* receive and send messages */
 $input = json_decode(file_get_contents('php://input'), true);
+
 if (isset($input['entry'][0]['messaging'][0]['sender']['id'])) {
 
-    $sender = $input['entry'][0]['messaging'][0]['sender']['id']; //sender facebook id
-    $message = $input['entry'][0]['messaging'][0]['message']['text']; //text that user sent
+    $sender = $input['entry'][0]['messaging'][0]['sender']['id']; //sender facebook id 
 
     $url = 'https://graph.facebook.com/v2.6/me/messages?access_token='. $access_token;
 
@@ -27,20 +28,22 @@ if (isset($input['entry'][0]['messaging'][0]['sender']['id'])) {
     $ch = curl_init($url);
 
     /*prepare response*/
-    $jsonData = '{
-    "recipient":{
-        "id":"' . $sender . '"
-        },
-        "message":{
-            "text": "OK"
-        }
-    }';
+    $message = 'What sup man';
+
+    $resp = array(
+      'recipient' => array(
+        'id' => $sender
+      ),
+      'message' => array(
+        'text' => $message
+      )
+    );
+    $jsonData = json_encode($resp);
 
     /* curl setting to send a json post data */
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-    if (!empty($message)) {
-        $result = curl_exec($ch); // user will get the message
-    }
+
+    $result = curl_exec($ch); // user will get the message
 }
