d1a4132c39362bac935c875a845445b879fc336b
kengdev <48957697+kengdev@users.noreply.github.com>
2021-10-30 13:07:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,3 +13,36 @@ if (isset($_GET['hub_verify_token'])) {
         return;
     }
 }
+
+/* receive and send messages */
+$input = json_decode(file_get_contents('php://input'), true);
+
+if (isset($input['entry'][0]['messaging'][0]['sender']['id'])) {
+
+    $sender = $input['entry'][0]['messaging'][0]['sender']['id']; //sender facebook id 
+
+    $url = 'https://graph.facebook.com/v2.6/me/messages?access_token='. $access_token;
+
+    /*initialize curl*/
+    $ch = curl_init($url);
+
+    /*prepare response*/
+    $message = 'What sup man';
+
+    $resp = array(
+      'recipient' => array(
+        'id' => $sender
+      ),
+      'message' => array(
+        'text' => $message
+      )
+    );
+    $jsonData = json_encode($resp);
+
+    /* curl setting to send a json post data */
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+
+    $result = curl_exec($ch); // user will get the message
+}
